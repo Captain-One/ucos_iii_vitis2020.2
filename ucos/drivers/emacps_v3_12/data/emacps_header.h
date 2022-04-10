372e0feaa2b150ddee8f9d1ca049557e0829b9e2
Captain-One <pxqjoe@sina.com>
2022-04-11 00:20:03 +0800

    创建仓库，适用与vitis2020.2,并添加lwip协议栈的移植

---
+++ ucos/drivers/emacps_v3_12/data/emacps_header.h
@@ -0,0 +1,23 @@
+/******************************************************************************
+* Copyright (C) 2011 - 2020 Xilinx, Inc.  All rights reserved.
+* SPDX-License-Identifier: MIT
+******************************************************************************/
+
+#ifndef EMACPS_HEADER_H		/* prevent circular inclusions */
+#define EMACPS_HEADER_H		/* by using protection macros */
+
+#include "xil_types.h"
+#include "xil_assert.h"
+#include "xstatus.h"
+
+#ifdef XPAR_INTC_0_DEVICE_ID
+int EmacPsDmaIntrExample(XIntc *IntcInstancePtr,
+			  XEmacPs *EmacPsInstancePtr,
+			  u16 EmacPsDeviceId, u16 EmacPsIntrId);
+#else
+int EmacPsDmaIntrExample(XScuGic *IntcInstancePtr,
+			  XEmacPs *EmacPsInstancePtr,
+			  u16 EmacPsDeviceId, u16 EmacPsIntrId);
+#endif
+
+#endif
---
+++ ucos/drivers/emacps_v3_12/src/xemacps_g.c
@@ -0,0 +1,69 @@
+/******************************************************************************
+* Copyright (C) 2010 - 2020 Xilinx, Inc.  All rights reserved.
+* SPDX-License-Identifier: MIT
+******************************************************************************/
+
+/*****************************************************************************/
+/**
+*
+* @file xemacps_g.c
+* @addtogroup emacps_v3_12
+* @{
+*
+* This file contains a configuration table that specifies the configuration of
+* ethernet devices in the system.
+*
+* <pre>
+* MODIFICATION HISTORY:
+*
+* Ver   Who  Date     Changes
+* ----- ---- -------- -----------------------------------------------
+* 1.00a wsy  01/10/10 First release
+* 2.00  hk   22/01/14 Added check for picking second instance
+* 3.00  kvn  02/13/15 Modified code for MISRA-C:2012 compliance.
+* </pre>
+*
+* @internal
+*
+* This configuration table contains entries that are modified at runtime by the
+* driver. This table reflects only the hardware configuration of the device.
+* This emac configuration table contains software information in addition to
+* hardware configuration.
+*
+******************************************************************************/
+
+/***************************** Include Files *********************************/
+
+#include "xemacps.h"
+#include "xparameters.h"
+
+/************************** Constant Definitions *****************************/
+
+
+/***************** Macros (Inline Functions) Definitions *********************/
+
+
+/************************** Function Prototypes ******************************/
+
+
+/************************** Variable Prototypes ******************************/
+
+/*
+ * The configuration table for emacps device
+ */
+
+XEmacPs_Config XEmacPs_ConfigTable[XPAR_XEMACPS_NUM_INSTANCES] = {
+	{
+		(u16)XPAR_XEMACPS_0_DEVICE_ID,  /* Device ID */
+		(UINTPTR)XPAR_XEMACPS_0_BASEADDR,    /* Device base address */
+		(u16)XPAR_XEMACPS_0_IS_CACHE_COHERENT,  /* Cache Coherency */
+	},
+#ifdef XPAR_XEMACPS_1_DEVICE_ID
+	{
+		(u16)XPAR_XEMACPS_1_DEVICE_ID,  /* Device ID */
+		(UINTPTR)XPAR_XEMACPS_1_BASEADDR,    /* Device base address */
+		(u16)XPAR_XEMACPS_1_IS_CACHE_COHERENT,  /* Cache coherency */
+	}
+#endif
+};
+/** @} */
